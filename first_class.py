8a896b470f6dbefa0ce1cd1ab62ddf700cc3be03
Esteban Jimenez <estebanjimenezsotelo457@gmail.com>
2025-12-12 14:22:58 -0500

    primer commit de python

---
+++ first_class.py
@@ -0,0 +1,102 @@
+# Mensaje en la terminal, en este caso es "Hola mundo"...
+print ("Hola mundo")
+
+# Varibles
+type_int = 5 # Tipo entero...
+type_float = 5.00 # Tipo decimal o real...
+type_strings = "Hola" # Tipo texto...
+
+# Operaciones 
+#print ("Esto es una suma")
+#numero_uno = 2
+#numero_dos = 4
+#resultado = numero_uno + numero_dos
+#print(resultado)
+
+# Cadena de caracteres (Strings)
+#mensaje = "Hola"
+#mensaje += " " # += Vas a sumar contenido a la varible sin borrar nada...
+#mensaje += "Hernesto"
+#print(mensaje) # Resultado "Hola Hernesto"...
+               # Si pusieras = en vez de += se borraria el contenido de la variable
+
+# Concatenacion (unir dos cadenas o mas "+")
+#mensaje = "Hola"
+#espacio = " "
+#nombre = "Hernesto"
+#print (mensaje + espacio + nombre) #Resultado "Hola Hernesto"
+
+# Con numeros
+#numero_uno = 5
+#numero_dos = 5
+#resultado = numero_uno + numero_dos
+#resultado = str(resultado) # Se tiene que convertir el int a string porque si no da error...
+#print ("El resultado de la suma es " + resultado) 
+                                                 # Concatenar int con stringe no se puede...
+
+# Busqueda (find) localizar dentro de una cadena, una sub cadena mas pequeña a un caracter
+#mensaje = "Hola Hernesto"
+#buscar_cadena = mensaje.find("Hernesto") # Busca por posicion que comienza desde cero y da resultado el nuemro de la poscion que esta el string
+#print(buscar_cadena) # Resultado da el numero de poscion de string que en este caso es 5
+            
+# Extraccion (extraer porciones de la cadena)
+mensaje = "Hola Hernesto"
+extraer_subcadena = mensaje[1:8] # Comienza a recorrer caracter por caracter de 1 a 8 y lo extrae pero hasta la posicion numero 7 
+print (extraer_subcadena) # Resultado "ola Her"
+
+# Comparacion == (para comparar dos cadenas de caracteres)
+mensaje_uno = "Hola"
+mensaje_dos = "Hola"
+print (mensaje_uno = mensaje_dos) # Los comprar a ver si son iguales o no y el resualtado puede ser True o False
+
+# Palabras reservadas 
+#and 
+#del 
+#for
+#is 
+#raise
+#assert
+#if 
+#else
+#elif
+#from
+#labda
+#return
+#break
+#global
+#not
+#try
+#class
+#exept
+#or
+#while
+#continue
+#exec
+#import
+#yield
+#def 
+#finally
+#in
+#print
+
+# Operadores aritmeticos 
+# Suma +
+# Resta -
+# Multiplicacion *
+# Exponente **
+# Modulo %
+# Division /
+# Division entera // 
+
+print ("suma: ")
+numero_uno = 5
+numero_dos = 4
+resultado = numero_uno + numero_dos
+resultado = str (resultado)
+print("El resultado de la suma es: " + resultado)
+
+
+
+
+
+
